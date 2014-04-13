6878f8339445a51e4aff8e2cff9da7920a526a59
Tom Eugelink <tbee@tbee.org>
2014-04-13 21:34:08 +0200

    fixed Label wrapText issue

--- javafx/src/test/java/org/tbee/javafx/scene/layout/test/MigPaneTest13.java
+++ javafx/src/test/java/org/tbee/javafx/scene/layout/test/MigPaneTest13.java
@@ -12,57 +12,62 @@ import org.tbee.javafx.scene.layout.MigPane;
 
 /**
  * The window should wrap the button perfectly (including the inset).
+ * 
  * @author Tom Eugelink
  *
  */
 public class MigPaneTest13 extends Application {
 
-    public static void main(String[] args) {
-    	launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 	@Override
-    public void start (Stage stage) throws Exception {
+	public void start(Stage stage) throws Exception {
 
-        final LabelBuilder<?> lBuilder = LabelBuilder
-                .create()
-                .text("Test long label to see if the wrap works ok in a Migpane. I am"
-                        + " going to have to keep writing because this may not be long enough yet!!")
-                .wrapText(true);
+		final LabelBuilder<?> lBuilder = LabelBuilder
+				.create()
+				.text("Test long label to see if the wrap works ok in a Migpane. I am going to have to keep writing because this may not be long enough yet!!")
+				.wrapText(true);
 
-        final VBox root = new VBox();
-        // we give a fixed width of 400 and a height of -1. The latter one means that the height of the Scene should be
-        // calculated by the bounds of its root 
-        final Scene scene = new Scene(root, 400, -1);
+		final VBox root = new VBox();
+		// we give a fixed width of 400 and a height of -1. The latter one means
+		// that the height of the Scene should be
+		// calculated by the bounds of its root
+		final Scene scene = new Scene(root, 400, -1);
 
-        final VBox vBox = new VBox();
-        LabelBuilder<?> titleLB = LabelBuilder.create().style(
-                "-fx-font-weight: bold;");
-        vBox.getChildren().add(titleLB.text("VBox").build());
-        final Label vBoxLabel = lBuilder.build();
-        vBox.getChildren().add(vBoxLabel);
-        root.getChildren().add(vBox);
+		// VBOX
+		final VBox vBox = new VBox();
+		LabelBuilder<?> titleLB = LabelBuilder.create().style("-fx-font-weight: bold;");
+		vBox.getChildren().add(titleLB.text("VBox").build());
+		final Label vBoxLabel = lBuilder.build();
+		vBox.getChildren().add(vBoxLabel);
+		root.getChildren().add(vBox);
 
-        root.getChildren().add(new Separator());
+		root.getChildren().add(new Separator());
 
-        final MigPane mPane = new MigPane("fill, ins 0 , gap 0");
-        final Label migLabel = lBuilder.build();
-        // doesn't even work when min height == pref height
-        mPane.add(titleLB.text("MigPane").build(), "wrap, hmin pref");
-        mPane.add(migLabel, "grow");
-        root.getChildren().add(mPane);
+		// MigPane
+		final MigPane mPane = new MigPane("fill, ins 0 , gap 0");
+		final Label migLabel = lBuilder.build();
+		// doesn't even work when min height == pref height
+		mPane.add(titleLB.text("MigPane").build(), "wrap");
+		mPane.add(migLabel, "grow");
+		root.getChildren().add(mPane);
 
-        stage.setScene(scene);
-        stage.show();
+		stage.setScene(scene);
+		stage.show();
 
-        // The label correctly calculetes its pref and max height based on the number of lines.
-        // Eg setting wrapText to false returns its minWidth (height for 1 line) as prefWidth and cuts the rest of the
-        // line with ellipsis, setting it to true returns a prefHeight so that every line is visible
-        double sceneWidth = scene.getWidth();
-        System.out.println("label heights for a scene width of " + sceneWidth
-                + ":\nmin: " + vBoxLabel.minHeight(sceneWidth) + ", pref: "
-                + vBoxLabel.prefHeight(sceneWidth) + ", max:"
-                + vBoxLabel.maxHeight(sceneWidth));
-        stage.sizeToScene();
-    }
+		// The label correctly calculetes its pref and max height based on the
+		// number of lines.
+		// Eg setting wrapText to false returns its minWidth (height for 1 line)
+		// as prefWidth and cuts the rest of the
+		// line with ellipsis, setting it to true returns a prefHeight so that
+		// every line is visible
+		double sceneWidth = scene.getWidth();
+		System.out.println("label heights for a scene width of " + sceneWidth
+				+ ":\nmin: " + vBoxLabel.minHeight(sceneWidth) + ", pref: "
+				+ vBoxLabel.prefHeight(sceneWidth) + ", max:"
+				+ vBoxLabel.maxHeight(sceneWidth));
+		stage.sizeToScene();
+	}
 }
