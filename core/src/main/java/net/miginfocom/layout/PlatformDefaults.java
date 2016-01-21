79783d669ec9ec0c9fe49c2716d0fd5e0cd549b8
Karl von Randow <karl@cactuslab.com>
2016-01-22 10:42:33 +1300

    Move getMinimumButtonWidthIncludingPadding from UnitValue to PlatformDefaults

--- core/src/main/java/net/miginfocom/layout/PlatformDefaults.java
+++ core/src/main/java/net/miginfocom/layout/PlatformDefaults.java
@@ -489,6 +489,16 @@ public final class PlatformDefaults
 	{
 		return BUTT_PADDING;
 	}
+    
+	public static float getMinimumButtonWidthIncludingPadding(float refValue, ContainerWrapper parent, ComponentWrapper comp)
+	{
+		final int buttonMinWidth = getMinimumButtonWidth().getPixels(refValue, parent, comp);
+		if (getMinimumButtonPadding() != null) {
+			return Math.max(comp.getMinimumWidth(comp.getWidth()) + getMinimumButtonPadding().getPixels(refValue, parent, comp) * 2, buttonMinWidth);
+		} else {
+			return buttonMinWidth;
+		}
+	}
 
 	/** Returns the unit value associated with the unit. (E.i. "related" or "indent"). Must be lower case.
 	 * @param unit The unit string.
